a54e987b324823bea6b21969effb7247464a0307
kimkablitz <kim.tran549@gmail.com>
2018-08-20 14:51:47 -0700

    Hangman-Game

---
+++ game.js
@@ -0,0 +1,38 @@
+
+var lives;
+document.getElementById("p1").innerHTML = lives;
+function getLives
+
+
+
+
+//var Answers = {2002:"Someonelikeyou",2010:"Makeyoufeelmylove",2012:"Payphone",2014:"chasingpavemnts",2017:"Girls"};
+//var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+// lives count, running from 6 to 0
+
+
+//var lives =["6","5","4","3","2","1","0"];
+    // for (var i = 0; i < lives.length; i++) {
+    // console.log(lives[i]);
+    // }
+    // function countLives()
+
+    // //Get elements
+    // var showlives=
+    // document.getElementById("livesNumber")= showLives;
+    // //showlives
+    // var lives;
+    // comments = function () {
+    //     showLives.innerHTML = "You have " + lives + " lives";
+    //     if (lives < 1) {
+    //     showLives.innerHTML = "Game Over";
+    //     }
+    //     for (var i = 0; i < geusses.length; i++) {
+    //     if (counter + space === geusses.length) {
+    //         showLives.innerHTML = "You Win!";
+    //     }
+    //     }
+    // }
+    // lives = 6;
